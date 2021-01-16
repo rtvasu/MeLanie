b9da69c1b6c36438cc1dd50c8bde308d25084255
Aarti Vasudevan <aartidevan@gmail.com>
2021-01-16 13:08:54 -0500

    installed packages, loaded model, put in img skele

---
+++ app/model/load.py
@@ -0,0 +1,14 @@
+import numpy as np
+import keras.models
+from keras.models import model_from_json
+from scipy.misc import imread, imresize,imshow
+
+import tensorflow as tf
+import efficientnet.tfkeras
+from tensorflow import keras
+
+
+def init():
+    model = tf.keras.models.load_model('/content/model2.h5')
+    graph = tf.get_default_graph()
+	return model,graph
--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 import os
 from app import app
 
+#image libraries
+from PIL import Image
+import cv2
+import numpy as np
+
 from flask import render_template, request, redirect
 
 @app.route("/")
@@ -15,6 +20,8 @@ def about():
     <code>Flask is <em>awesome</em></code>
     """
 
+
+
 app.config["IMAGE_UPLOADS"] = "/mnt/c/wsl/projects/pythonise/tutorials/flask_series/app/app/static/img/uploads"
 @app.route("/upload-image", methods=["GET", "POST"])
 def upload_image():
@@ -26,9 +33,18 @@ def upload_image():
             #now that we have the image, we should be able to send it to the model
             image = request.files["image"]
 
-            print(image)
+            read = lambda imname: np.asarray(Image.open(imname).convert("RGB"))
+            img = read(image)
+            img = cv2.resize(img, (224,224))
+
+            img = np.array(img)/255.
+            img = np.array(img)/255.
+            img = np.reshape(img,[1,224,224,3])
+            
+            #print(image)
 
             return redirect(request.url)
 
 
-    return render_template("public/upload_image.html")
+    
+    return render_template("public/upload_image.html", user_image = image) if (request.method == "POST") else render_template("public/upload_image.html")
