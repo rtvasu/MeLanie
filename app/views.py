85fa4ee292a69e61a7d5b7ced5f71a169b79aec5
Aarti Vasudevan <aartidevan@gmail.com>
2021-01-16 02:12:55 -0500

    allows uploading image on button click

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
+import os
 from app import app
 
-from flask import render_template
+from flask import render_template, request, redirect
 
 @app.route("/")
 def index():
@@ -13,3 +14,21 @@ def about():
     <p>This is a lovely little paragraph</p>
     <code>Flask is <em>awesome</em></code>
     """
+
+app.config["IMAGE_UPLOADS"] = "/mnt/c/wsl/projects/pythonise/tutorials/flask_series/app/app/static/img/uploads"
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+
+    if request.method == "POST":
+
+        if request.files:
+
+            #now that we have the image, we should be able to send it to the model
+            image = request.files["image"]
+
+            print(image)
+
+            return redirect(request.url)
+
+
+    return render_template("public/upload_image.html")
